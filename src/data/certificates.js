6f7dda7e838f8035a4725aede20bce8c5981244b
Windi Saputra <saputrawindi0@gmail.com>
2025-04-27 09:54:44 +0700

    Update Portofolio Website

--- src/data/certificates.js
+++ src/data/certificates.js
@@ -4,29 +4,28 @@ const certificates = [
       title: "Java Spring Boot - ReactJs - React Native",
       issuer: "PT Enigma Cipta Humanika",
       date: "March 2025",
-      imageUrl: "/src/assets/certificate/enigma.jpg",
-    //   credentialUrl: "https://example.com/verify/123"
+      imageUrl: "/certificates/enigma.jpg",
     },
     {
         id: 2,
         title: "Intro to Software Engineering",
         issuer: "PT Revolusi Cita Edukasi",
         date: "October 2024",
-        imageUrl: "/src/assets/certificate/revou.jpg",
+        imageUrl: "/certificates/revou.jpg",
       },
       {
         id: 3,
         title: "Responsive Web Design",
         issuer: "freeCodeCamp.org",
         date: "November 2021",
-        imageUrl: "/src/assets/certificate/rwd.png",
+        imageUrl: "/certificates/rwd.png",
       },
     {
       id: 4,
       title: "Legacy JavaScript Algorithm and Data Structures",
       issuer: "freeCodeCamp.org",
       date: "April 2021",
-      imageUrl: "/src/assets/certificate/ds.png",
+      imageUrl: "/certificates/ds.png",
     },
     
     
